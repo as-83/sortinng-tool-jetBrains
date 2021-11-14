67a83b25d5dbb09d303177a7e166a26ad4506012
Suleyman hp <abdsul3434@gmail.com>
2021-11-14 15:56:19 +0300

    first stage completed. Long input sorting and counting

--- Sorting Tool/task/src/sorting/Main.java
+++ Sorting Tool/task/src/sorting/Main.java
@@ -5,10 +5,19 @@ import java.util.*;
 public class Main {
     public static void main(final String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        List<Long> longList = new ArrayList<>();
         while (scanner.hasNextLong()) {
             long number = scanner.nextLong();
-            // write your code here
+            longList.add(number);
         }
+
+        Collections.sort(longList);
+        int totalNumbers = longList.size();
+        long greatestNumber = longList.get(totalNumbers - 1);
+        long time = longList.stream().filter(l -> l == greatestNumber).count();
+
+
+        System.out.printf("Total numbers: %d.\n" +
+                "The greatest number: %d (%d time(s)).", totalNumbers, greatestNumber, time);
     }
 }

04c273c489726cece71176937a8a8333eb259b9b
Suleyman hp <abdsul3434@gmail.com>
2021-11-14 18:34:44 +0300

    Second stage frame

--- Sorting Tool/task/src/sorting/Main.java
+++ Sorting Tool/task/src/sorting/Main.java
@@ -3,8 +3,31 @@ package sorting;
 import java.util.*;
 
 public class Main {
+    private static Scanner scanner = new Scanner(System.in);
     public static void main(final String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        String dataType = "word";
+
+        if (args.length > 0 && args[0].equals( "-dataType")) {
+            dataType = args[1];
+        }
+
+        switch (dataType) {
+            case "long": longParser(); break;
+            case "word": wordParser(); break;
+            case "line": lineParser(); break;
+            default: break;
+        }
+
+
+    }
+
+    private static void lineParser() {
+    }
+
+    private static void wordParser() {
+    }
+
+    private static void longParser() {
         List<Long> longList = new ArrayList<>();
         while (scanner.hasNextLong()) {
             long number = scanner.nextLong();
